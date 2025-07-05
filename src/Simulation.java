cdd05a8bdd236c71d1e693cf1586e936c22f6bf4
errih anes <anes.errih@etu.univ-lyon1.fr>
2025-07-05 23:30:40 +0200

    add Simulate Class

---
+++ src/Simulation.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+
+public class Simulation {
+    private Forest forest;
+    private double probability;
+    private Random random;
+
+    public Simulation(Forest _forest, double _probability) {
+        this.forest = _forest;
+        this.probability = _probability;
+        this.random = new Random();
+
+    }
+    private boolean hasFire(){
+        for (int y = 0; y< forest.getHeight(); y++){
+            for (int x = 0; x< forest.getWidth(); x++){
+                if(forest.getCell(x, y).isFire()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Forest copyForest(Forest _forest){
+        Forest copy = new Forest(forest.getWidth(), forest.getHeight());
+        for (int y = 0; y< forest.getHeight(); y++){
+            for (int x = 0; x< forest.getWidth(); x++){
+                copy.getCell(x,y).setState(forest.getCell(x,y).getState());
+            }
+        }
+        return copy;
+    }
+    private void Propagate(int x, int y, Forest newForest) {
+
+        int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+
+        int randomPercent = random.nextInt(100);
+        for (int[] dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+
+
+            if (forest.isPositionValid(newX, newY) &&
+                    newForest.getCell(newX, newY).isVegetation() &&
+                    random.nextDouble() < probability) {
+
+                newForest.setFire(newX, newY);
+            }
+        }
+    }
+    public void simulate() {
+        int step = 1;
+
+        while (hasFire()){
+            step++;
+            System.out.println("Étape " + step + " :");
+
+            Forest newForest = copyForest(forest);
+
+            for(int y = 0; y < newForest.getHeight(); y++){
+                for(int x = 0; x < newForest.getWidth(); x++){
+
+                    if(forest.getCell(x, y).isFire()){
+                        newForest.getCell(x,y).setOnAsh();
+
+                        Propagate(x, y, newForest);
+                    }
+                }
+            }
+            this.forest = newForest;
+            forest.display();
+
+        }
+        System.out.println("étape " + step + " terminée:");
+    }
+
+}
